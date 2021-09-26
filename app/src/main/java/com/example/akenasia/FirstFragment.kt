a9ae65b2c6ef072b9e88f488ae92b263b4838ab7
Kaanar <sefkanbeyaz@gmail.com>
2021-09-26 14:35:47 +0200

    -m "Première version de l'activité Main"

--- app/src/main/java/com/example/akenasia/FirstFragment.kt
+++ app/src/main/java/com/example/akenasia/FirstFragment.kt
@@ -1,44 +1,47 @@
 package com.example.akenasia
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.example.akenasia.databinding.FragmentFirstBinding
+import android.widget.TextView
+
+
+
+
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
-        return binding.root
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false)
 
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
-    }
+        view.findViewById<TextView>(R.id.textview_XCoordonnees_Current_Value).text = "0"
+        view.findViewById<TextView>(R.id.textview_YCoordonnees_Current_Value).text = "0"
+        view.findViewById<TextView>(R.id.textview_XCoordonnees_Goals_Value).text = "48.890900"
+        view.findViewById<TextView>(R.id.textview_YCoordonnees_Goals_Value).text = "2.209300"
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+        view.findViewById<Button>(R.id.button_first).setOnClickListener {
+            //https://www.webrankinfo.com/forum/t/calcul-des-coordonnees-gps-dans-un-rayon-de-x-km.74024/
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+
+
+        }
     }
 }
