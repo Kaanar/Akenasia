4ee27c9fe9e1e0270f60e4205ecd4516d2af82ae
Kaanar <sefkanbeyaz@gmail.com>
2021-09-28 00:10:55 +0200

    -m "Test pour Travis.ci"

--- app/src/main/java/com/example/akenasia/MainActivity.kt
+++ app/src/main/java/com/example/akenasia/MainActivity.kt
@@ -41,7 +41,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
+        // Handle action bar item clicks here. The action bar will //
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
