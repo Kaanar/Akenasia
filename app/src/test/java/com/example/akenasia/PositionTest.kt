12e820b9ad4d3fb4a86ce58583f9b23921aa34ac
PaulGorlicki <paul.gorlicki@gmail.com>
2021-10-10 16:00:15 +0200

    Full commit

---
+++ app/src/test/java/com/example/akenasia/PositionTest.kt
@@ -0,0 +1,17 @@
+package com.example.akenasia
+
+import org.junit.jupiter.api.Assertions.*
+
+import org.junit.jupiter.api.Test
+
+internal class PositionTest {
+
+    @Test
+    fun getLatitude() {
+
+    }
+
+    @Test
+    fun getLongitude() {
+    }
+}
---
+++ app/src/test/java/com/example/akenasia/UserTest.kt
@@ -0,0 +1,16 @@
+package com.example.akenasia
+
+import org.junit.jupiter.api.Assertions.*
+
+import org.junit.jupiter.api.Test
+
+internal class UserTest {
+
+    @Test
+    fun getUserId() {
+    }
+
+    @Test
+    fun getUserName() {
+    }
+}
