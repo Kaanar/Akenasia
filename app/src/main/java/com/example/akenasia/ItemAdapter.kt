6b6aec08ad481135ebc2f8ca248e36b26a5245c9
cathy <cathy.siwatala@gmail.com>
2021-11-23 20:40:14 +0100

    ItemAdapter

--- app/src/main/java/com/example/akenasia/ItemAdapter.kt
+++ app/src/main/java/com/example/akenasia/ItemAdapter.kt
@@ -1,34 +1,38 @@
+
 package com.example.akenasia
 
 import android.annotation.SuppressLint
 import android.app.Activity
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.BaseAdapter
+import android.widget.ImageView
 import android.widget.TextView
 
-class ItemAdapter(private val context: Activity, private val id: Array<String>, private val name: Array<String>, private val desc: Array<String>) : ArrayAdapter<String>(context, R.layout.bag_cardview, id) {
-    lateinit var inflater: LayoutInflater
-    lateinit var DbContext: DatabaseHandler
-
-    @SuppressLint("ViewHolder")
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-
-        val inflater = context.layoutInflater
-        val rowView = inflater.inflate(R.layout.bag_cardview, null, true)
-
-        //Lie les éléments xml à l'adapter
-        val idText = rowView.findViewById(R.id.id) as TextView
-        val nameText = rowView.findViewById(R.id.name) as TextView
-        val descText = rowView.findViewById(R.id.description) as TextView
-
-        //remplit les TextView par les valeurs des positions
-        idText.text = (id[position].toInt()+1).toString()
-        nameText.text = name[position]
-        descText.text = desc[position]
-
-        return rowView
+class ItemAdapter(var context:Context, var arrayList: ArrayList<Item>) : BaseAdapter() {
+    override fun getCount(): Int {
+        return arrayList.size
     }
+
+    override fun getItem(position: Int): Any {
+        return arrayList.get(position)
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        var view:View = View.inflate(context,R.layout.bag_cardview,null)
+
+        var icons:ImageView = view.findViewById(R.id.grid_img)
+        var names:TextView =view.findViewById(R.id.grid_tv)
+
+        return view
+    }
+
 
 }
