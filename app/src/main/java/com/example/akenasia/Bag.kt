602ce8f040b0a41eed5a817a9d2a6bc55d288077
Kaanar <sefkanbeyaz@gmail.com>
2021-11-23 17:35:10 +0100

    Modif de bag en activité

--- app/src/main/java/com/example/akenasia/Bag.kt
+++ app/src/main/java/com/example/akenasia/Bag.kt
@@ -6,23 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.akenasia.databinding.ActivityOpenworldBinding
 import com.example.akenasia.databinding.BagCardviewBinding
 import com.example.akenasia.databinding.ChronometreBinding
 
-class Bag : Fragment() {
-    private var _binding: BagCardviewBinding? = null
+class Bag : AppCompatActivity() {
+    private lateinit var binding: BagCardviewBinding
 
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        if (container != null) {
-            val id= this.arguments?.getInt("id")
-        }
-
-        _binding = BagCardviewBinding.inflate(inflater, container, false)
-        return _binding!!.root
-
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = BagCardviewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
--- app/src/main/java/com/example/akenasia/OpenWorld.kt
+++ app/src/main/java/com/example/akenasia/OpenWorld.kt
@@ -2,29 +2,39 @@ package com.example.akenasia
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Chronometer
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.commit
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 
 import com.example.akenasia.databinding.ActivityOpenworldBinding
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.PointOfInterest
 
 
-class OpenWorld : AppCompatActivity() {
+class OpenWorld : AppCompatActivity(),OnMapReadyCallback, GoogleMap.OnPoiClickListener {
 
 
     private lateinit var pos: Position
     private var isPlay: Boolean = false
     private lateinit var binding: ActivityOpenworldBinding
     private lateinit var appBarConfiguration: AppBarConfiguration
-
+    private lateinit var googleMap: GoogleMap
+    private lateinit var chronometre: Chronometer
+    private var cameraFocus: Boolean = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityOpenworldBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
 
         //setSupportActionBar(binding.toolbar)
         val navHostFragment =
@@ -46,19 +56,73 @@ class OpenWorld : AppCompatActivity() {
                         true
                     }
                     R.id.BagClick -> {
-                        if (navController != null) {
-                            navController.navigate(R.id.map_fragment_to_bag_fragment)
-                        }
-                        else Toast.makeText(this, "pas de navcontroller",Toast.LENGTH_LONG).show()
-
+                        val intent = Intent(this, Bag::class.java)
+                        this.startActivity(intent)
                         true
                     }
                     else -> {
-                        Toast.makeText(this, "fuck off",Toast.LENGTH_LONG).show()
+                        Toast.makeText(this, "Available soon",Toast.LENGTH_LONG).show()
                        true
                     }
                 }
             true
         }
+    }
+    override fun onMapReady(map: GoogleMap) {
+        map?.let {
+            googleMap = it
+            googleMap.setOnPoiClickListener(this)
+
+            val location1= LatLng(48.905273887110944, 2.2156870365142827)
+            val location2 = LatLng(48.904096168019976, 2.216480970382691)
+            val location3 = LatLng(48.903158204219174, 2.2155475616455083)
+            googleMap.addMarker(MarkerOptions().position(location1).title("BU"))
+            googleMap.addMarker(MarkerOptions().position(location2).title("Crous"))
+            googleMap.addMarker(MarkerOptions().position(location3).title("Bat G"))
+
+            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location1,17f))
+            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location2,17f))
+            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location3,17f))
+
+            //Vérifie chaque tick du chrono
+            chronometre.onChronometerTickListener = Chronometer.OnChronometerTickListener {
+                val currentTime: String = chronometre.getText().toString()
+                val location= LatLng(pos.getLatitude(), pos.getLongitude(),)
+                googleMap.clear()
+                googleMap.addMarker(MarkerOptions().position(location).title("Position"))
+                if (cameraFocus) {
+                    googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location,15f))
+                }
+            }
+        }
+
+    }
+
+    override fun onPoiClick(poi: PointOfInterest) {
+        pos.refreshLocation()
+        //calcul de la distance
+        val distance: Double = pos.calcul_distance(
+            pos.getLatitude(),
+            pos.getLongitude(),
+            poi.latLng.latitude,
+            poi.latLng.longitude,
+        )
+        if (distance < 500) {
+            var dialog = PoiDialog()
+            dialog.setName(updateTitle(poi))
+            dialog.setLatLong(updateInfo(poi))
+            //dialog.show(parentFragmentManager, "PoiDialog") //ça pareil ça compile pas
+        }
+        else {
+            Toast.makeText(this, "Trop loin", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    fun updateTitle(poi: PointOfInterest) : String {
+        return poi.name
+    }
+
+    fun updateInfo(poi: PointOfInterest) : String {
+        return poi.latLng.latitude.toString() +"\n" + poi.latLng.longitude.toString()
     }
 }
