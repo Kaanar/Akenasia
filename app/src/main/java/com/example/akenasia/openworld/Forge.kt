39b68068f072da95b052bde30459289aaffbfb2b
PaulGorlicki <paul.gorlicki@gmail.com>
2022-01-19 11:00:47 +0100

    ListItems Forge

--- app/src/main/java/com/example/akenasia/openworld/Forge.kt
+++ app/src/main/java/com/example/akenasia/openworld/Forge.kt
@@ -1,26 +1,34 @@
 package com.example.akenasia.openworld
 
+import ItemAdapter
 import android.content.Intent
 import android.os.Bundle
+import android.widget.CompoundButton
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
+import com.example.akenasia.Handler.DatabaseHandler
 import com.example.akenasia.Handler.ItemHandler
 import com.example.akenasia.R
+import com.example.akenasia.database.Item
+import com.example.akenasia.database.ListItems
 import com.example.akenasia.databinding.ForgeBinding
 import com.example.akenasia.home.MainActivity
+import kotlinx.android.synthetic.main.forge.*
+import kotlinx.android.synthetic.main.regles_generales.*
+import java.util.ArrayList
 
 
 class Forge :  AppCompatActivity() {
     private lateinit var binding: ForgeBinding
-
-    private lateinit var itemHandler : ItemHandler
-
+    private lateinit var itemHandler: ItemHandler
+    private lateinit var type : ListItems
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ForgeBinding.inflate(layoutInflater)
         setContentView(binding.root)
         itemHandler = ItemHandler(applicationContext)
-
+        classicRecord()
         //Implémentation des différents choix du menu
         binding.NavigationView.setOnItemSelectedListener { item ->
             when (item.itemId) {
@@ -43,5 +51,83 @@ class Forge :  AppCompatActivity() {
             }
             true
         }
+
+        binding.CBEpee.setOnClickListener {
+            if(CBEpee.isChecked) {
+                type = ListItems.EPEE
+                searchRecord()
+            }
+            else {
+                classicRecord()
+            }
+        }
+
+        binding.CBBouclier.setOnClickListener {
+            if(CBBouclier.isChecked) {
+                type = ListItems.BOUCLIER
+                searchRecord()
+            }
+            else {
+                classicRecord()
+            }
+        }
+
+        binding.CBChaussures.setOnClickListener {
+            if(CBChaussures.isChecked) {
+                type = ListItems.CHAUSSURES
+                searchRecord()
+            }
+            else {
+                classicRecord()
+            }
+        }
+
+        binding.CBArmure.setOnClickListener {
+            if(CBArmure.isChecked) {
+                type = ListItems.ARMURE
+                searchRecord()
+            }
+            else {
+                classicRecord()
+            }
+        }
     }
+
+    private fun classicRecord() {
+        //creating the instance of DatabaseHandler class
+        val databaseHandler = DatabaseHandler(applicationContext)
+        //calling the viewPlace method of DatabaseHandler class to read the records
+        val emp: List<Item> = itemHandler.view()
+        viewRecord(emp)
+    }
+
+    private fun searchRecord() {
+        val databaseHandler = DatabaseHandler(applicationContext)
+        val emp: List<Item> = itemHandler.viewByType(type)
+        viewRecord(emp)
+    }
+
+    private fun viewRecord(emp : List<Item>) {
+        val empArrayId = Array(emp.size) { "0" }
+        val empArrayName = Array(emp.size) { "null" }
+        val empArrayDesc = Array(emp.size) { "null" }
+        val empArrayAtt = Array(emp.size) { "null" }
+        val empArrayDef = Array(emp.size) { "null" }
+
+        var index = 0
+        for (e in emp) {
+            empArrayId[index] = e.Itemid.toString()
+            empArrayName[index] = e.ItemName
+            empArrayDesc[index] = e.ItemDesc
+            empArrayAtt[index] = e.ItemAtt.toString()
+            empArrayDef[index] = e.ItemDef.toString()
+            index++
+        }
+        //creating custom ArrayAdapter
+        val myListAdapter =
+            ItemAdapter(this, empArrayId, empArrayName, empArrayDesc, empArrayAtt, empArrayDef)
+        ListViewItemForge?.adapter = myListAdapter
+
+    }
+
 }
