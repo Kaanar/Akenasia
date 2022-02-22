602a5ce963c052e97fa8cd4044d44ef2fab98822
cathy <cathy.siwatala@gmail.com>
2022-02-22 09:35:05 +0100

    UpHp / UpPointdeVie

--- app/src/main/java/com/example/akenasia/Handler/PersonnageHandler.kt
+++ app/src/main/java/com/example/akenasia/Handler/PersonnageHandler.kt
@@ -138,7 +138,28 @@ class PersonnageHandler(var context: Context): Handler {
         return success
     }
 
+    fun UpHP(emp:Int, hp: Double): Int{
+        val db = dbHandler.writableDatabase
+        val contentValues = ContentValues()
+        val personnage= this.get(emp)
 
+        contentValues.put(KEY_ID, emp)
+        contentValues.put(KEY_HP,personnage.persoHp+hp)
+        contentValues.put(KEY_ATT,personnage.persoAtt)
+        contentValues.put(KEY_DEF, personnage.persoDef)
+        contentValues.put(KEY_ARMURE,personnage.armure)
+        contentValues.put(KEY_BOUCLIER, personnage.bouclier)
+        contentValues.put(KEY_EPEE, personnage.epee)
+        contentValues.put(KEY_CHAUSSURES, personnage.chaussures)
+        contentValues.put(KEY_POINT,0)
+        contentValues.put(KEY_LEVEL, personnage.level+1)
+
+        // Updating Row
+        val success = db.update(dbHandler.TABLE_PERSONNAGE, contentValues,"id= $emp ",null)
+        //2nd argument is String containing nullColumnHack
+        db.close() // Closing database connection
+        return success
+    }
 
 
     //method to update your Personnage's affected items
